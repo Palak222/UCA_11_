fa5ce0ca1365a1dc37b2519970ec136e6ec8c144
Palak222 <80598844+Palak222@users.noreply.github.com>
2021-08-28 23:52:22 +0530

    commit

---
+++ reverse.c
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseBetween(struct ListNode* head, int left, int right){
+    
+    struct ListNode *p, *q;
+    p = q = head;
+    
+    while(left < right)
+    {
+        int i = 1, j = 1;
+        while(i < left)
+        {
+            p = p -> next;
+            i++;
+        }
+        while(j < right)
+        {
+            q = q -> next;
+            j++;
+        }
+        int temp = p -> val;
+        p -> val = q -> val;
+        q -> val = temp;
+        
+        left++;
+        right--;
+        
+        p = p -> next;
+        q = head;
+    }
+    
+    return head;
+
+}
